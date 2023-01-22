c8957a58927ac3be179414b64a3f87265748158a
Nitsan <nitsan.soffair@gmail.com>
2023-01-22 11:23:32 +0200

    .

--- BFF.py
+++ BFF.py
@@ -7,7 +7,7 @@ sensor_signals = np.random.rand(10, 1000)
 fft_sensor_signals = np.fft.fft(sensor_signals, axis=1)
 
 # Multiply each sensor signal's FFT by a phase shift factor
-phase_shift_factor = np.exp(1j*np.random.rand(10, 1000))
+phase_shift_factor = np.exp(1j * np.random.rand(10, 1000))
 shifted_fft_sensor_signals = fft_sensor_signals * phase_shift_factor
 
 # Sum the phase-shifted sensor signals
